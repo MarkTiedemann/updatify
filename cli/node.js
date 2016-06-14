7b86e8706acd495086a98f60c99d0e3f792a90b3
Mark Tiedemann <www.marktiedemann@gmail.com>
2016-06-14 10:55:47 +0300

    Implements updating node for v1.0.0

--- cli/node.js
+++ cli/node.js
@@ -1,7 +1,45 @@
 'use strict'
 
+const { node } = require('node-latest')
+const semver = require('semver')
+const msi = require('node-msi')
+
 module.exports = (args, spinner) => {
 
-    spinner.success('node')
+    const current = node.current()
 
+    const fetchUpdate = ({ tag, version }) => {
+        spinner.update(`Downloading Node installer (previous:`
+            + ` ${current} --> ${tag}: ${version})`)
+        msi.fetch({ version, bits: args.bits, dir: args.dir })
+            .then(path => args.download
+                ? spinner.success(`Node installer downloaded succesfully (previous:`
+                    + ` ${current} --> ${tag}: ${version}):\n${path}`)
+                : msi.start(path)
+                    .then(() => spinner.success(`Node installer started succesfully`
+                        + ` (previous: ${current} --> ${tag}: ${version})`))
+            )
+            .catch(err => spinner.fail(err))
+    }
+
+    const checkUpdate = tag => node[tag]()
+        .then(version => semver.gt(version, current)
+            ? args.check
+                ? spinner.fail(`Node needs to be updated `
+                    + `(installed: ${current} < ${tag}: ${version})`)
+                : fetchUpdate({ tag, version })
+            : semver.eq(version, current)
+                ? spinner.success(`Node is up to date (installed: `
+                    + `${current} == ${tag}: ${version})`)
+                : spinner.success(`Node is up to date (installed: `
+                    + `${current} > ${tag}: ${version})`))
+        .catch(err => spinner.fail(err))
+
+    spinner.update('Fetching metadata')
+
+    args.latest
+        ? checkUpdate('latest')
+        : args.lts
+            ? checkUpdate('lts')
+            : checkUpdate('latest')
 }
